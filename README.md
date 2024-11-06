222e9af86659cfc59fc65588391b69c2e8b68dac
Dasdron15 <ogurechik760@gmail.com>
2024-11-07 00:49:36 +0300

    Added the rest of the commands

---
+++ README.md
@@ -0,0 +1,3 @@
+# CLI To Do List App
+
+Simple to do list command line program, with a bunch of commands.
--- main.py
+++ main.py
@@ -19,21 +19,67 @@ def update_task(id, name):
     with open("task_list.json", "r") as f:
         data = json.load(f)
 
-    for i in data["tasks"]:
-        if i["id"] == str(id):
-            print(i)
+    try:
+        data["tasks"][id - 1].update({"name": name})
+
+        with open("task_list.json", "w") as f:
+            json.dump(data, f, indent=4)
+
+        print(f"Task {id} has been updated")
+    except:
+        print("No task with such id")
 
 def delete_task(id):
-    print(f"Task number {id} has been deleted")
+    with open("task_list.json", "r") as f:
+        data = json.load(f)
+
+    try:
+        data["tasks"].pop(id - 1)
+
+        with open("task_list.json", "w") as f:
+            json.dump(data, f, indent=4)
+
+        print(f"Task {id} has been deleted")
+    except:
+        print("No task with such id")
 
 def in_progress(id):
-    print(f"Updated status of task number {id} to in progress")
+    with open("task_list.json", "r") as f:
+        data = json.load(f)
+
+    try:
+        data["tasks"][id - 1].update({"status": "In progress"})
+
+        with open("task_list.json", "w") as f:
+            json.dump(data, f, indent=4)
+
+        print("Status has been updated to In progress")
+    except:
+        print("No task with such id")
 
 def done(id):
-    print(f"Marked task number {id} as done")
+    with open("task_list.json", "r") as f:
+        data = json.load(f)
 
-def task_list(status):
-    print("Showing all the tasks")
+    try:
+        data["tasks"][id - 1].update({"status": "Done"})
+
+        with open("task_list.json", "w") as f:
+            json.dump(data, f, indent=4)
+
+        print("Status has been updated to Done")
+    except:
+        print("No task with such id")
+
+def task_list():
+    with open("task_list.json", "r") as f:
+        data = json.load(f)
+
+    if len(data["tasks"]) == 0:
+        print("Task list is empty")
+    else:
+        for i in data["tasks"]:
+            print(f"Id: {i["id"]}, Name: {i["name"]}, Task Status: {i["status"]}")
 
 while True:
     command = input("Enter command: ")
@@ -46,19 +92,29 @@ while True:
             update_task(int(command.split(" ")[1]), command.split('"')[1::2][0])
 
         elif command[:6] == "delete":
-            pass
+            delete_task(int(command.split(" ")[1]))
 
         elif command[:16] == "mark-in-progress":
-            pass
+            in_progress(int(command.split(" ")[1]))
 
         elif command[:9] == "mark-done":
-            pass
+            done(int(command.split(" ")[1]))
 
         elif command[:4] == "list":
-            pass
+            task_list()
+
+        elif command[:4] == "help":
+            print("""----------------
+add <task name>
+update <task id> <updated task name>
+delete <task id>
+mark-in-progress <task id>
+mark-done <task id>
+list <Optional: done, todo, in-progress>
+----------------""")
 
         else:
-            print("Unknown command")
+            print("Unknown command: type 'help' to view the list of commands")
             
     except Exception as e:
         print("Invalid syntax")
